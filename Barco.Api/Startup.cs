4bfd065bd4de1064dac1d4a879f87dc62496e26e
musharaf-mcd <mohammad.musharaf@us.mcd.com>
2022-05-15 22:36:32 +0530

    added reactjs project and folder structure

--- Barco.Api/Startup.cs
+++ Barco.Api/Startup.cs
@@ -14,6 +14,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BarcoSales.DAO;
+using Newtonsoft.Json.Serialization;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
 
 namespace Barco.Api
 {
@@ -30,12 +33,23 @@ namespace Barco.Api
         public void ConfigureServices(IServiceCollection services)
         {
 
+            //Enable CORS
+            services.AddCors(c =>
+            {
+                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
+
+            //JSON Serializer
+            services.AddControllersWithViews().AddNewtonsoftJson(options =>
+            options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore)
+                .AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver
+                = new DefaultContractResolver());
+
             services.AddControllers();
             services.AddHttpClient();
             services.AddSwaggerGen();
             services.AddDbContext<BarcoSalesCommissionContext>(options =>
             options.UseSqlServer(Configuration["DbConnection"]));
-
 
             services.AddTransient<ICustomerRepository, CustomerDAO>();
         }
@@ -43,6 +57,9 @@ namespace Barco.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //Enable CORS
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -60,6 +77,12 @@ namespace Barco.Api
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My Test1 Api v1");
+            });
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+                   Path.Combine(Directory.GetCurrentDirectory(), "Photos")),
+                RequestPath = "/Photos"
             });
         }
     }
